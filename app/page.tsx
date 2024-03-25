41013db5f9d8b0697c582a9afae73a78de615494
Sam Selikoff <sam.selikoff@gmail.com>
2024-03-24 22:13:26 -0400

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -4,57 +4,69 @@ import * as d3 from "d3";
 import { motion, useSpring, useTransform } from "framer-motion";
 import { useEffect, useState } from "react";
 
-let arc = d3
-  .arc()
-  .innerRadius((200 / 2) * 0.67)
-  .outerRadius(200 / 2 - 1);
+let arc = d3.arc();
+let pie = d3.pie().sort(null);
+
+let width = 200;
+let height = 200;
+
+let colors = ["#333", "#999", "#ccc"];
 
 export default function Home() {
-  let width = 200;
-  let height = 200;
-  let [data, setData] = useState([1, 3]);
+  let [data, setData] = useState([1, 2, 5]);
 
   function handleClick() {
-    setData([2, 3]);
+    if (data[0] === 1) {
+      setData([2, 5, 4]);
+    } else {
+      setData([1, 2, 5]);
+    }
   }
 
-  let pies = d3.pie()(data);
+  let pies = pie(data);
 
   return (
-    <div>
+    <div className="flex flex-col h-screen justify-center items-center">
       <svg
         viewBox={`${-width / 2} ${-height / 2} ${width} ${height}`}
         width={width}
         height={height}
       >
-        <Slice pie={pies[0]} fill="#333" />
-        <Slice pie={pies[1]} fill="#999" />
+        {pies.map((pie, i) => (
+          <Slice key={i} pie={pie} fill={colors[i]} />
+        ))}
       </svg>
 
-      <div className="mt-4">
-        <button onClick={handleClick}>Change data</button>
+      <div className="mt-8">
+        <button
+          className="rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          onClick={handleClick}
+        >
+          Change data
+        </button>
       </div>
     </div>
   );
 }
 
-function Slice({ pie, fill }) {
+function Slice({ pie, fill }: { pie: d3.PieArcDatum<number>; fill: string }) {
   let { startAngle, endAngle } = pie;
-  let animatedStartAngle = useSpring(startAngle);
-  let animatedEndAngle = useSpring(endAngle);
+  let animatedStartAngle = useSpring(startAngle, { bounce: 0 });
+  let animatedEndAngle = useSpring(endAngle, { bounce: 0 });
 
   useEffect(() => {
     animatedStartAngle.set(startAngle);
     animatedEndAngle.set(endAngle);
   }, [animatedEndAngle, animatedStartAngle, endAngle, startAngle]);
 
-  let d = useTransform(() => {
-    return arc({
-      ...pie,
+  let d = useTransform(() =>
+    arc({
       startAngle: animatedStartAngle.get(),
       endAngle: animatedEndAngle.get(),
-    });
-  });
+      innerRadius: (200 / 2) * 0.67,
+      outerRadius: 200 / 2 - 1,
+    })
+  );
 
   return <motion.path initial={false} style={{ d }} fill={fill} />;
 }
